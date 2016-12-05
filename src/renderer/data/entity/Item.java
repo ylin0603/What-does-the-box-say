4bc5c9562995e15ebd24af031212528b8a533ac3
Stanley Lin <stanley.lin@pvnetwork.ltd>
2016-12-05 16:28:37 +0800

    update construct
    add position of the Item

--- src/renderer/data/entity/Item.java
+++ src/renderer/data/entity/Item.java
@@ -10,10 +10,12 @@ public class Item extends Entity {
     private Boolean shared;
     private int owner;
 
-    public Item(String name, int index, Boolean shared) {
+    public Item(String name, int index, Boolean shared, int x, int y) {
         this.name = name;
         this.index = index;
         this.shared = shared;
+        this.x = x;
+        this.y = y;
         this.sprite = Sprite.ITEM;
     }
 
