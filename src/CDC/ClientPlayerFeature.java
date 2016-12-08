5eddd9a57867344a0f4e2ca183244a305d5037fd
lihungte96 <lihungte96@gmail.com>
2016-12-08 20:48:00 +0800

    codestyle of my part

--- src/CDC/ClientPlayerFeature.java
+++ src/CDC/ClientPlayerFeature.java
@@ -2,51 +2,51 @@ package CDC;
 
 public class ClientPlayerFeature {
 
-    private int playerId;
-    private int locationX, locationY;
-    private int direction = 0;
-    private int velocity = 2;
-    private String nickName = null;
+	private int playerId;
+	private int locationX, locationY;
+	private int direction = 0;
+	private int velocity = 2;
+	private String nickName = null;
 
-    public ClientPlayerFeature(int clientNo, String nickName) {
-        this.playerId = clientNo;
-        this.nickName = nickName;
-    }
+	public ClientPlayerFeature(int clientNo, String nickName) {
+		this.playerId = clientNo;
+		this.nickName = nickName;
+	}
 
-    public void setLocationX(int locationX) {
-        this.locationX = locationX;
-    }
+	public void setLocationX(int locationX) {
+		this.locationX = locationX;
+	}
 
-    public void setLocationY(int locationY) {
-        this.locationY = locationY;
-    }
+	public void setLocationY(int locationY) {
+		this.locationY = locationY;
+	}
 
-    public void setDirection(int direction) {
-        this.direction = direction;
-    }
+	public void setDirection(int direction) {
+		this.direction = direction;
+	}
 
-    public void setVelocity(int velocity) {
-        this.velocity = velocity;
-    }
+	public void setVelocity(int velocity) {
+		this.velocity = velocity;
+	}
 
-    public int getPlayerId() {
-        return playerId;
-    }
+	public int getPlayerId() {
+		return playerId;
+	}
 
-    public int getLocationX() {
-        return locationX;
+	public int getLocationX() {
+		return locationX;
 
-    }
+	}
 
-    public int getLocationY() {
-        return locationY;
-    }
+	public int getLocationY() {
+		return locationY;
+	}
 
-    public int getDirection() {
-        return direction;
-    }
+	public int getDirection() {
+		return direction;
+	}
 
-    public int getVelocity() {
-        return velocity;
-    }
+	public int getVelocity() {
+		return velocity;
+	}
 }
--- src/tcp/tcpServer/RealTcpServer.java
+++ src/tcp/tcpServer/RealTcpServer.java
@@ -6,63 +6,63 @@ import java.net.Socket;
 import java.util.ArrayList;
 
 public class RealTcpServer implements Runnable {
-    final int port = 8888;
-    ServerSocket ss;
-    static int totalClient = 0;
-    volatile ArrayList<String> IPtable = new ArrayList<String>();
-    private static RealTcpServer realTcpServer;
+	final int port = 8888;
+	ServerSocket ss;
+	static int totalClient = 0;
+	volatile ArrayList<String> IPtable = new ArrayList<String>();
+	private static RealTcpServer realTcpServer;
 
-    private RealTcpServer() {
+	private RealTcpServer() {
 
-    }
+	}
 
-    public static RealTcpServer getInstance() {
-        if (realTcpServer == null) {
-            realTcpServer = new RealTcpServer();
-        }
-        return realTcpServer;
-    }
+	public static RealTcpServer getInstance() {
+		if (realTcpServer == null) {
+			realTcpServer = new RealTcpServer();
+		}
+		return realTcpServer;
+	}
 
-    public void initTCPServer() {
-        try {
-            ss = new ServerSocket(port);
-            System.out
-                    .println("server" + ss.getLocalSocketAddress().toString());
-            Thread t = new Thread(this);
-            t.start();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
+	public void initTCPServer() {
+		try {
+			ss = new ServerSocket(port);
+			System.out
+					.println("server" + ss.getLocalSocketAddress().toString());
+			Thread t = new Thread(this);
+			t.start();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
-    public ArrayList<String> getClientIPTable() {
-        return IPtable;
-    }
+	public ArrayList<String> getClientIPTable() {
+		return IPtable;
+	}
 
-    @Override
-    public void run() {
-        // TODO Auto-generated method stub
-        while (true) {
-            // every server thread
-            Socket so;
-            try {
-                so = ss.accept();
-                String s1 = so.getInetAddress().toString().replace("/", "");
-                IPtable.add(s1);
-                TcpServerThread tcpServerThread =
-                        new TcpServerThread(so, totalClient);
-                if (totalClient == 0) {
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		while (true) {
+			// every server thread
+			Socket so;
+			try {
+				so = ss.accept();
+				String s1 = so.getInetAddress().toString().replace("/", "");
+				IPtable.add(s1);
+				TcpServerThread tcpServerThread =
+						new TcpServerThread(so, totalClient);
+				if (totalClient == 0) {
 
-                }
-                Thread t = new Thread(tcpServerThread);
-                t.start();
-                totalClient++;
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-                break;
-            }
-        }
-    }
+				}
+				Thread t = new Thread(tcpServerThread);
+				t.start();
+				totalClient++;
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				break;
+			}
+		}
+	}
 }
