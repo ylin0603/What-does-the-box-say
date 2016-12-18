f37fb1ed15b46c3a02741d43409109cd03f01ff5
lihungte96 <lihungte96@gmail.com>
2016-12-18 23:39:33 +0800

    all collision

--- src/CDC/Collision.java
+++ src/CDC/Collision.java
@@ -3,45 +3,56 @@ package CDC;
 public class Collision {
     final static double PlayerSize = Cdc.BOX_SIZE / 2;
 
-    // for normal player collision
-    // for normal attack
+    // no use
     public static boolean isCollison(ClientPlayerFeature player1,
             ClientPlayerFeature player2) {
-        double size = ((PlayerSize * 2) * 1.2);
         int p1LocX = player1.getLocX();
         int p1LocY = player1.getLocY();
         int p2LocX = player2.getLocX();
         int p2LocY = player2.getLocY();
-        if (distance1d(p1LocX, p2LocX) > size
-                || distance1d(p1LocY, p2LocY) > size)
-            return false;
-        if (distance2d(p1LocX, p1LocY, p2LocX, p2LocY) < Math.pow(size, 2))
-            return true;
-        return false;
+        return isCollison(p1LocX, p1LocY, p2LocX, p2LocY);
     }
 
+    // for attack player
     public static boolean isCollison(ClientItemFeature item,
             ClientPlayerFeature player2) {
-        double size = ((PlayerSize * 2) * 1.2);
         int p1LocX = item.getLocX();
         int p1LocY = item.getLocY();
         int p2LocX = player2.getLocX();
         int p2LocY = player2.getLocY();
-        if (distance1d(p1LocX, p2LocX) > size
-                || distance1d(p1LocY, p2LocY) > size)
-            return false;
-        if (distance2d(p1LocX, p1LocY, p2LocX, p2LocY) < Math.pow(size, 2))
-            return true;
-        return false;
+        return isCollison(p1LocX, p1LocY, p2LocX, p2LocY);
     }
 
+    // for normal player collision
+    public static boolean isCollison(int p1LocX, int p1LocY,
+            ClientPlayerFeature player2) {
+        int p2LocX = player2.getLocX();
+        int p2LocY = player2.getLocY();
+        return isCollison(p1LocX, p1LocY, p2LocX, p2LocY);
+    }
+
+    // for attack box
     public static boolean isCollison(ClientItemFeature item1,
             ClientItemFeature item2) {
-        double size = ((PlayerSize * 2) * 1.2);
         int p1LocX = item1.getLocX();
         int p1LocY = item1.getLocY();
         int p2LocX = item2.getLocX();
         int p2LocY = item2.getLocY();
+        return isCollison(p1LocX, p1LocY, p2LocX, p2LocY);
+    }
+
+    // for respawn box
+    public static boolean isCollison(int p1LocX, int p1LocY,
+            ClientItemFeature item2) {
+        int p2LocX = item2.getLocX();
+        int p2LocY = item2.getLocY();
+        return isCollison(p1LocX, p1LocY, p2LocX, p2LocY);
+    }
+
+    // for all
+    public static boolean isCollison(int p1LocX, int p1LocY, int p2LocX,
+            int p2LocY) {
+        double size = ((PlayerSize * 2) * 1.2);
         if (distance1d(p1LocX, p2LocX) > size
                 || distance1d(p1LocY, p2LocY) > size)
             return false;
@@ -50,8 +61,6 @@ public class Collision {
         return false;
     }
 
-    // for box player collision
-    // for eat pack
     // for bullet attack
     public static boolean isCirclePlayerCollison(int c1LocX, int c1LocY,
             int c1Size, ClientPlayerFeature player2) {
