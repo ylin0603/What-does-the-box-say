77ca2888997e15f046e52c97cf5e2c7463d7759e
Bear <william456821@gmail.com>
2016-12-22 22:24:30 +0800

    短距離攻擊 四個方位 測試

--- src/CDC/test/AttackTest.java
+++ src/CDC/test/AttackTest.java
@@ -39,26 +39,72 @@ public class AttackTest {
                 new ArrayList<ClientPlayerFeature>();
         clientPlayerFeature.add(new ClientPlayerFeature(0, "bbb", 100, 100));
         clientPlayerFeature.add(new ClientPlayerFeature(1, "bbb", 5000, 5000));
-        int oriBlood = clientPlayerFeature.get(1).getHP();
+        int oriBlood;
         // clientItemFeature.add(new ClientItemFeature(1,1, 210, 210));// id =1 ,假箱子
         ClientPlayerFeature player = clientPlayerFeature.get(0);
+        ClientPlayerFeature attackedPlayer = clientPlayerFeature.get(1);
         player.setWeaponType(0); // 0 是近戰
 
         // case1 距離太遠打不到
-        player.setFaceAngle(0);
+        oriBlood = attackedPlayer.getHP();
         Attack test = new Attack(player, clientPlayerFeature, clientItemFeature,
                 clientBulletFeature);
-        assertEquals(oriBlood, clientPlayerFeature.get(1).getHP());
+        assertEquals(oriBlood, attackedPlayer.getHP());
         // (16+16*1.2=35.2 是攻擊距離) 故 但測出來 100-33 = 67 是 極限
-        // case2 打的到 在 玩家0 北方 被打到
-        clientPlayerFeature.get(1).setLocX(100);
-        clientPlayerFeature.get(1).setLocY(67);
-        oriBlood = clientPlayerFeature.get(1).getHP();
+
+        // case2 打的到 在 玩家0 正北方 被打到
+        player.setFaceAngle(0);
+        attackedPlayer.setLocX(100);
+        attackedPlayer.setLocY(67);
+        oriBlood = attackedPlayer.getHP();
         test = new Attack(player, clientPlayerFeature, clientItemFeature,
                 clientBulletFeature);
-        System.out.println(clientPlayerFeature.get(1).getHP());
-        assertEquals(oriBlood, clientPlayerFeature.get(1).getHP());
+        System.out.println(attackedPlayer.getHP());
+        assertEquals(oriBlood - 80, attackedPlayer.getHP());
 
+        // case3 打的到 在 玩家0 正東方 被打到
+        player.setFaceAngle(90);
+        attackedPlayer.setHP(100);
+        attackedPlayer.setLocX(133);
+        attackedPlayer.setLocY(100);
+        oriBlood = attackedPlayer.getHP();
+        test = new Attack(player, clientPlayerFeature, clientItemFeature,
+                clientBulletFeature);
+        System.out.println(attackedPlayer.getHP());
+        assertEquals(oriBlood - 80, attackedPlayer.getHP());
+
+        // case4 打的到 在 玩家0 正東方 被打到
+        player.setFaceAngle(90);
+        attackedPlayer.setHP(100);
+        attackedPlayer.setLocX(133);
+        attackedPlayer.setLocY(100);
+        oriBlood = attackedPlayer.getHP();
+        test = new Attack(player, clientPlayerFeature, clientItemFeature,
+                clientBulletFeature);
+        System.out.println(attackedPlayer.getHP());
+        assertEquals(oriBlood - 80, attackedPlayer.getHP());
+
+        // case5 打的到 在 玩家0 正南方 被打到
+        player.setFaceAngle(180);
+        attackedPlayer.setHP(100);
+        attackedPlayer.setLocX(100);
+        attackedPlayer.setLocY(133);
+        oriBlood = attackedPlayer.getHP();
+        test = new Attack(player, clientPlayerFeature, clientItemFeature,
+                clientBulletFeature);
+        System.out.println(attackedPlayer.getHP());
+        assertEquals(oriBlood - 80, attackedPlayer.getHP());
+
+        // case6 打的到 在 玩家0 正西方 被打到
+        player.setFaceAngle(270);
+        attackedPlayer.setHP(100);
+        attackedPlayer.setLocX(67);
+        attackedPlayer.setLocY(100);
+        oriBlood = attackedPlayer.getHP();
+        test = new Attack(player, clientPlayerFeature, clientItemFeature,
+                clientBulletFeature);
+        System.out.println(attackedPlayer.getHP());
+        assertEquals(oriBlood - 80, attackedPlayer.getHP());
 
     }
 
