789d0a44302aa17a01566d764b1492e7f26d8735
Bear <william456821@gmail.com>
2016-12-22 22:01:31 +0800

    扣血扣兩次

--- src/CDC/test/AttackTest.java
+++ src/CDC/test/AttackTest.java
@@ -37,16 +37,29 @@ public class AttackTest {
                 new ArrayList<ClientBulletFeature>();
         ArrayList<ClientPlayerFeature> clientPlayerFeature =
                 new ArrayList<ClientPlayerFeature>();
-        clientPlayerFeature.add(new ClientPlayerFeature(0, "bbb", 0, 0));
+        clientPlayerFeature.add(new ClientPlayerFeature(0, "bbb", 100, 100));
         clientPlayerFeature.add(new ClientPlayerFeature(1, "bbb", 5000, 5000));
         int oriBlood = clientPlayerFeature.get(1).getHP();
         // clientItemFeature.add(new ClientItemFeature(1,1, 210, 210));// id =1 ,假箱子
         ClientPlayerFeature player = clientPlayerFeature.get(0);
         player.setWeaponType(0); // 0 是近戰
+
+        // case1 距離太遠打不到
         player.setFaceAngle(0);
         Attack test = new Attack(player, clientPlayerFeature, clientItemFeature,
                 clientBulletFeature);
         assertEquals(oriBlood, clientPlayerFeature.get(1).getHP());
+        // (16+16*1.2=35.2 是攻擊距離) 故 但測出來 100-33 = 67 是 極限
+        // case2 打的到 在 玩家0 北方 被打到
+        clientPlayerFeature.get(1).setLocX(100);
+        clientPlayerFeature.get(1).setLocY(67);
+        oriBlood = clientPlayerFeature.get(1).getHP();
+        test = new Attack(player, clientPlayerFeature, clientItemFeature,
+                clientBulletFeature);
+        System.out.println(clientPlayerFeature.get(1).getHP());
+        assertEquals(oriBlood, clientPlayerFeature.get(1).getHP());
+
+
     }
 
 }
