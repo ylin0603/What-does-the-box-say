0cfa8e9b543f6e67c03659c396868a33959235f5
twinkle8s <twinklecindy8s@gmail.com>
2016-12-22 21:15:27 +0800

    add input ip

--- src/gui/gameInterface/JoinGamePanel.java
+++ src/gui/gameInterface/JoinGamePanel.java
@@ -13,12 +13,21 @@ public class JoinGamePanel extends JPanel {
 
     private JFrame frame = BeforeGameManager.getInstance().getFrame();
     private RealTcpClient realTcpClient;
-    private String name;
+    private String name, serverIp;
 
     public JoinGamePanel() {
-        name = JOptionPane.showInputDialog(frame, "Please input your nickname:",
+        name = JOptionPane.showInputDialog(frame, "Please input your nickname :",
                 "Join Room", JOptionPane.QUESTION_MESSAGE);
         if (name == null || name.isEmpty())
+            BeforeGameManager.getInstance().addInitialPanel();
+        else
+            askServerIp();
+    }
+    
+    private void askServerIp() {
+        serverIp = JOptionPane.showInputDialog(frame, "Please input server ip:",
+                "Server IP", JOptionPane.QUESTION_MESSAGE);
+        if (serverIp == null || serverIp.isEmpty())
             BeforeGameManager.getInstance().addInitialPanel();
         else
             connectServer();
@@ -26,7 +35,7 @@ public class JoinGamePanel extends JPanel {
 
     private void connectServer() {
         realTcpClient = BeforeGameManager.getInstance().getTcpClient();
-        boolean isConnect = realTcpClient.connectServer("127.0.0.1");
+        boolean isConnect = realTcpClient.connectServer(serverIp);
         if (!isConnect) {
             JOptionPane.showMessageDialog(frame, "Connect fail!!!",
                     "Connect Fail", JOptionPane.ERROR_MESSAGE);
