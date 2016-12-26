5263f2aea034dee819e0a5d29ce07fe152d919df
Bear <william456821@g.ncu.edu.tw>
2016-12-26 16:17:42 +0800

    attack test bullet

--- src/CDC/test/AttackTest.java
+++ src/CDC/test/AttackTest.java
@@ -11,6 +11,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import CDC.Attack;
+import CDC.Cdc;
 import CDC.ClientBulletFeature;
 import CDC.ClientItemFeature;
 import CDC.ClientPlayerFeature;
@@ -43,7 +44,11 @@ public class AttackTest {
     }
 
     @After
-    public void tearDown() throws Exception {}
+    public void tearDown() throws Exception {
+        clientItemFeature = null;
+        clientBulletFeature = null;
+        clientPlayerFeature = null;
+    }
 
     @Test
     public void testAttackShortRangeNorth() {
@@ -57,8 +62,8 @@ public class AttackTest {
         oriBlood = attackedPlayer.getHP();
         test = new Attack(player, clientPlayerFeature, clientItemFeature,
                 clientBulletFeature);
-        // System.out.println(attackedPlayer.getHP());
         assertEquals(oriBlood - 50, attackedPlayer.getHP());
+        assertEquals(false, attackedPlayer.isDead());
 
         // case2.1 打不到 玩家0 在正北方
         attackedPlayer.setHP(100);
@@ -88,6 +93,7 @@ public class AttackTest {
         test = new Attack(player, clientPlayerFeature, clientItemFeature,
                 clientBulletFeature);
         assertEquals(oriBlood, attackedPlayer.getHP());
+        assertEquals(false, attackedPlayer.isDead());
     }
 
     @Test
@@ -103,7 +109,7 @@ public class AttackTest {
         oriBlood = attackedPlayer.getHP();
         test = new Attack(player, clientPlayerFeature, clientItemFeature,
                 clientBulletFeature);
-        // System.out.println(attackedPlayer.getHP());
+
         assertEquals(oriBlood - 50, attackedPlayer.getHP());
     }
 
@@ -120,7 +126,7 @@ public class AttackTest {
         oriBlood = attackedPlayer.getHP();
         test = new Attack(player, clientPlayerFeature, clientItemFeature,
                 clientBulletFeature);
-        // System.out.println(attackedPlayer.getHP());
+
         assertEquals(oriBlood - 50, attackedPlayer.getHP());
     }
 
@@ -137,24 +143,170 @@ public class AttackTest {
         oriBlood = attackedPlayer.getHP();
         test = new Attack(player, clientPlayerFeature, clientItemFeature,
                 clientBulletFeature);
-        // System.out.println(attackedPlayer.getHP());
+
         assertEquals(oriBlood - 50, attackedPlayer.getHP());
     }
 
     @Test
-    public void testAttackShortRangeFake() {
+    public void testAttackShortRangeFakeBox() {
         int oriBlood;
         player.setWeaponType(0); // 0 是近戰
         // case7 假箱子 假設他在玩家正北方 打到 扣 60
         clientItemFeature.add(new ClientItemFeature(0, 0, 100, 67));// id =1 ,假箱子
         player.setFaceAngle(0);
         oriBlood = player.getHP();
-        assertEquals(false, clientItemFeature.get(0).isDead());
+        assertEquals(false, player.isDead());
         test = new Attack(player, clientPlayerFeature, clientItemFeature,
                 clientBulletFeature);
         assertEquals(oriBlood - 60, player.getHP());
-        assertEquals(true, clientItemFeature.get(0).isDead());
+        assertEquals(false, player.isDead());
 
     }
 
+    @Test
+    public void testAttackBulletRangeY() {// 測試攻擊距離Y
+        player.setWeaponType(1);
+        player.setFaceAngle(180);
+        attackedPlayer.setLocX(100);
+        attackedPlayer.setLocY(259);
+        attackedPlayer.setHP(100);
+        test = new Attack(player, clientPlayerFeature, clientItemFeature,
+                clientBulletFeature);
+        for (int i = 0; i < 30; i++) {
+            new Attack().attackLongRangeUpdate(clientPlayerFeature,
+                    clientItemFeature, clientBulletFeature);
+        }
+        assertEquals(60, attackedPlayer.getHP());
+        assertEquals(0, clientBulletFeature.size());
+        // 這是在極限距離外
+        attackedPlayer.setLocX(100);
+        attackedPlayer.setLocY(260);
+        attackedPlayer.setHP(100);
+        test = new Attack(player, clientPlayerFeature, clientItemFeature,
+                clientBulletFeature);
+        for (int i = 0; i < 31; i++) {
+            new Attack().attackLongRangeUpdate(clientPlayerFeature,
+                    clientItemFeature, clientBulletFeature);
+        }
+        assertEquals(100, attackedPlayer.getHP());
+        assertEquals(0, clientBulletFeature.size());
+    }
+
+    @Test
+    public void testAttackBulletRangeX() {// 測試攻擊距離X
+        player.setWeaponType(1); // 1 是遠攻
+        player.setFaceAngle(90);
+        // 在幾乎極限距離被打到 飛了
+        attackedPlayer.setLocX(419);
+        attackedPlayer.setLocY(100);
+        attackedPlayer.setHP(100);
+        test = new Attack(player, clientPlayerFeature, clientItemFeature,
+                clientBulletFeature);
+        for (int i = 0; i < 30; i++) {
+            new Attack().attackLongRangeUpdate(clientPlayerFeature,
+                    clientItemFeature, clientBulletFeature);
+        }
+        assertEquals(60, attackedPlayer.getHP());
+        assertEquals(0, clientBulletFeature.size());
+        // 這是在極限距離外
+        attackedPlayer.setLocX(420);
+        attackedPlayer.setLocY(100);
+        attackedPlayer.setHP(100);
+        test = new Attack(player, clientPlayerFeature, clientItemFeature,
+                clientBulletFeature);
+        for (int i = 0; i < 31; i++) {
+            new Attack().attackLongRangeUpdate(clientPlayerFeature,
+                    clientItemFeature, clientBulletFeature);
+        }
+        assertEquals(100, attackedPlayer.getHP());
+        assertEquals(0, clientBulletFeature.size());
+    }
+
+    @Test
+    public void testAttackBulletRangeAtBorderY() {
+
+        player.setWeaponType(1); // 1 是遠攻
+        player.setFaceAngle(180);
+        player.setLocX(0);
+        player.setLocY(0);
+        // 在幾乎極限距離被打到 飛了 120 的距離
+        attackedPlayer.setLocX(0);
+        attackedPlayer.setLocY(50);
+        attackedPlayer.setHP(100);
+
+        test = new Attack(player, clientPlayerFeature, clientItemFeature,
+                clientBulletFeature);
+        for (int i = 0; i < 31; i++) {
+            new Attack().attackLongRangeUpdate(clientPlayerFeature,
+                    clientItemFeature, clientBulletFeature);
+        }
+        assertEquals(60, attackedPlayer.getHP());
+
+    }
+
+    @Test
+    public void testAttackBulletRangeAtBorderX() {
+        player.setWeaponType(1); // 1 是遠攻
+        player.setFaceAngle(90);
+        player.setLocX(0);
+        player.setLocY(0);
+        // 在幾乎極限距離被打到 飛了 120 的距離
+        attackedPlayer.setLocX(20);
+        attackedPlayer.setLocY(0);
+        attackedPlayer.setHP(100);
+
+        test = new Attack(player, clientPlayerFeature, clientItemFeature,
+                clientBulletFeature);
+        for (int i = 0; i < 31; i++) {
+            new Attack().attackLongRangeUpdate(clientPlayerFeature,
+                    clientItemFeature, clientBulletFeature);
+        }
+        assertEquals(60, attackedPlayer.getHP());
+    }
+
+
+
+    @Test
+    public void testAttackBulletBorder() { // 射到邊界 子彈是否消失
+        player.setWeaponType(1); // 1 是遠攻
+        // 人在左下角靠近邊界
+        player.setLocX(Cdc.MAP_SIZE_X - 50);
+        player.setLocY(Cdc.MAP_SIZE_Y - 50);
+        player.setFaceAngle(90);
+        test = new Attack(player, clientPlayerFeature, clientItemFeature,
+                clientBulletFeature);
+        for (int i = 0; i < 5; i++) {
+            new Attack().attackLongRangeUpdate(clientPlayerFeature,
+                    clientItemFeature, clientBulletFeature);
+        }
+        assertEquals(0, clientBulletFeature.size());
+    }
+
+    @Test
+    public void testAttackBulletAngle() {
+        // 被攻擊腳色在南邊
+        player.setWeaponType(1); // 1 是遠攻
+        attackedPlayer.setLocX(100);
+        attackedPlayer.setLocY(220);
+
+        player.setFaceAngle(170);
+        attackedPlayer.setHP(100);
+        test = new Attack(player, clientPlayerFeature, clientItemFeature,
+                clientBulletFeature);
+        for (int i = 0; i < 31; i++) {
+            new Attack().attackLongRangeUpdate(clientPlayerFeature,
+                    clientItemFeature, clientBulletFeature);
+        }
+        assertEquals(100, attackedPlayer.getHP());
+
+        player.setFaceAngle(190);
+        attackedPlayer.setHP(100);
+        test = new Attack(player, clientPlayerFeature, clientItemFeature,
+                clientBulletFeature);
+        for (int i = 0; i < 31; i++) {
+            new Attack().attackLongRangeUpdate(clientPlayerFeature,
+                    clientItemFeature, clientBulletFeature);
+        }
+        assertEquals(100, attackedPlayer.getHP());
+    }
 }
